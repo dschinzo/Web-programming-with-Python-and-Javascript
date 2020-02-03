34c8ed21af4c07d634f639617bdb3b26dd6b0af9
dschinzo <ds.chinzo@gmail.com>
2020-02-03 21:30:38 +0800

    set env var

--- lecture3/list.py
+++ lecture3/list.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("postgresql:///lecture3")
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
